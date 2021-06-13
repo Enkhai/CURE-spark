94cea4944de8dc6cf6067fbd307cf9066b12f9fb
Enkhai <aerogtx@gmail.com>
2021-06-14 00:12:40 +0300

    Deleting redundant project folder for Windows. Final changes.

--- Will-Karas/src/main/scala/clustering/structures/Cluster.scala
+++
@@ -1,9 +0,0 @@
-package clustering.structures
-
-case class Cluster(points: Array[KDPoint],
-                   var representatives: Array[KDPoint],
-                   var nearest: Cluster,
-                   var mean: KDPoint,
-                   var squaredDistance: Double = 0.0d,
-                   var id: Int = 0)
-
--- Will-Karas/src/main/scala/clustering/structures/KDNode.scala
+++
@@ -1,6 +0,0 @@
-package clustering.structures
-
-case class KDNode(point: KDPoint,
-                  var left: KDNode,
-                  var right: KDNode,
-                  var deleted: Boolean = false)
